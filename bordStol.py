8c725bf739e3ac9cb8235c07f53eaca788a0a2d9
atlem03 <atle.mogestad@edu.huddinge.se>
2025-11-20 09:43:51 +0000

    added bordStol pullup value

---
+++ bordStol.py
@@ -0,0 +1,6 @@
+print("Hello World!")
+sergio_pullup = 3
+atle_pullup = 10
+eskil_pullup = 9
+median_pullup = (atle_pullup + sergio_pullup + eskil_pullup)/2
+print("This is the median pullup value of the boys: " + str(median_pullup))
